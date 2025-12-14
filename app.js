dfa86ed386bf71376d6d60ac356800ee05a0b0ee
Enoqkip84 <enoq.kipchirchir@gmail.com>
2025-12-15 00:33:05 +0800

    files

--- app.js
+++ app.js
@@ -121,3 +121,30 @@ Closure - Example 1 (Easy)
 Closure - Example 2 (Hard one)
 ========================================================
 */
+
+function newAccount(name, initialBalance) {
+  let balance = initialBalance;
+  function showBalance() {
+    console.log(`Hey ${name}, your balance is ${balance}`);
+  }
+  function deposit(amount) {
+    balance += amount;
+    showBalance();
+  }
+  function withdraw(amount) {
+    if (balance < amount) {
+      console.log(`Hey ${name} your balance is insufficient`);
+      return;
+    }
+    balance -= amount;
+    showBalance();
+  }
+
+  return { showBalance: showBalance, deposit: deposit, withdraw: withdraw };
+}
+
+const john = newAccount('john', 300);
+const bob = newAccount('bob', 500);
+
+john.showBalance();
+bob.showBalance();
